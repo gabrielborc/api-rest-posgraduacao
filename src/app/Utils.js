f32ffc8f5b0d1353722bb25f59c629d2a4576bcc
Gabriel <gabrielborc@gmail.com>
2019-08-26 23:55:07 -0300

    Implementado os filtros de consulta contains e in

--- src/app/Utils.js
+++ src/app/Utils.js
@@ -35,7 +35,31 @@ class Utils {
         return newMock;
     }
 
+    filterContains(field, value, mock) {
+        let regex = new RegExp(value, 'i');
+        field = field.split('_')[0];
+
+        return mock.filter((item) => regex.test(item[field]));
+    }
+
+    filterIn(field, values, mock) {
+        field = field.split('_')[0];
+        values = values.split(',');
+
+        return mock.filter((item) => values.indexOf('' + item[field]) !== -1);        
+    }
+
     filterQuery(params, mock) {
+        let that = this;
+
+        Object.keys(params).forEach((param) => {
+            if (/_contains/.test(param)) {
+                mock = that.filterContains(param, params[param], mock);
+            } else if (/_in/.test(param)) {
+                mock = that.filterIn(param, params[param], mock);
+            }
+        });
+
         if (params.hasOwnProperty('fields')) {
             mock = this.filterFields(params.fields, mock);
         }
